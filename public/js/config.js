3b683595a9783dd4b89a7553785d504d5effca28
SabaloZua <sabalozua@outlook.pt>
2025-07-07 18:40:41 +0100

    primeiro comint

---
+++ public/js/config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const URLWS= process.env.URLWS ;
+const WS_URL = URLWS;
+
+const STATUS = {
+  WIN: "youwin",
+  LOSE: "youlose",
+};
+
+const ACTIONS = {
+  ADMIN: "admin",
+  DRAW: "draw",
+  CLIENT_COUNT_UPDATE: "clientCountUpdate",
+};
